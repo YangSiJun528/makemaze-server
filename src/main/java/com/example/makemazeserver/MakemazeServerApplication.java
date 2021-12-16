47e79a7764dff7fc460de960bdcb7fe3d4bf891c
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-12-16 13:55:40 +0900

    Initial commit

---
+++ src/main/java/com/example/makemazeserver/MakemazeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemazeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemazeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakemazeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makemazeserver/MakemazeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makemazeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemazeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
